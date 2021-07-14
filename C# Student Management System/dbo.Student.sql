8dc4eb8aee6af300ffe421bff0a8ed506a811d39
Pandula1234 <52339029+Pandula1234@users.noreply.github.com>
2021-07-14 13:19:44 +0530

    Student Management Portal
    Using Visual Studio Created a Project using Mysql Server 2012

---
+++ C# Student Management System/dbo.Student.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[StudentId] INT NOT NULL PRIMARY KEY, 
+    [FirstName] VARCHAR(50) NOT NULL, 
+    [Email] VARCHAR(50) NOT NULL, 
+    [Tp] INT NULL, 
+    [Gender] VARCHAR(50) NOT NULL, 
+    [Grade] VARCHAR(50) NOT NULL
+)
---
+++ C# Student Management System/dbo.Table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Table] (
+    [Id]         INT          NOT NULL,
+    [First Name] VARCHAR (50) NOT NULL,
+    [Email]      VARCHAR (50) NOT NULL,
+    [Telephone]  INT          NULL,
+    [Gender]     VARCHAR (50) NOT NULL,
+    [Grade]      VARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
