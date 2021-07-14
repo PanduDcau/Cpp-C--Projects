44368790469fe7db53e025aa57ce4bdf01dd9cae
Pandula1234 <52339029+Pandula1234@users.noreply.github.com>
2021-07-14 13:26:55 +0530

    C# Project files

---
+++ C# Student Management System/Components/bin/Debug/dbo.Table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Table] (
+    [StudentID]  INT          NOT NULL,
+    [First Name] VARCHAR (50) NOT NULL,
+    [Email]      VARCHAR (50) NOT NULL,
+    [Telephone]  INT          NULL,
+    [Gender]     VARCHAR (50) NULL,
+    [Grade]      VARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([StudentID] ASC)
+);
+
